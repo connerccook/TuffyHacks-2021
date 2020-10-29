f84fd13a984d323896d32bdda21efe7e3df1ac9a
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-10-28 21:12:22 -0700

    🌼

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react"
+import { useWindowWidth } from "@react-hook/window-size" // https://github.com/jaredLunde/react-hook/tree/master/packages/window-size#readme
+import { slide as Menu } from "react-burger-menu" // https://github.com/negomi/react-burger-menu
 import "../styles/Header.scss"
 
+const links: [string, string][] = [
+  ["About", "#about"],
+  ["Sponsors", "#sponsors"],
+  ["FAQ", "#faq"],
+]
+
 export default function Header() {
+  const windowWidth = useWindowWidth()
   return (
     <header>
       <h1>TuffyHacks</h1>
